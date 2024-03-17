8e9f20566cd0963d1b2c682a20f967b6a1ce1510
SergiuDumitru <85940124+SergiuDumitru@users.noreply.github.com>
2024-03-18 00:35:07 +0200

    top

---
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -0,0 +1,71 @@
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import 'devextreme/dist/css/dx.dark.css';
+import 'devextreme/dist/css/dx.light.css';
+import DataGrid, {
+  Column,
+  ColumnChooser,
+  ColumnFixing,
+} from "devextreme-react/data-grid";
+
+const UserWidget = ({ userId, picturePath }) => {
+  const [user, setUser] = useState(null);
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.token);
+  const dark = palette.neutral.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
+  const posts = useSelector((state) => state.posts);
+  
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) {
+    return null;
+  }
+
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
+
+  return (
+    <WidgetWrapper>
+      <DataGrid dataSource={posts} keyExpr="_id">
+        <ColumnChooser enabled={true} />
+        <ColumnFixing enabled={true} />
+        <Column dataField="firstName" dataType="firstName" />
+      </DataGrid>
+    </WidgetWrapper>
+  );
+};
+
+export default UserWidget;
